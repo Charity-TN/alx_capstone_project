6e4aabb3bd83d5c087319c1c77b0e13c4056bdb3
Charity Thuo <charitynthuo@gmail.com>
2025-04-03 23:13:25 +0300

    Created the user model.

--- library_management_api/api/models.py
+++ library_management_api/api/models.py
@@ -8,3 +8,12 @@ class Book(models. Model):
     isbn = models.IntegerField(max_length=13,unique=True)
     published_date = models.DateField()
     copies_available = models.PositiveIntegerField(default=1)
+
+class User(models.Model):
+    user_id= models.AutoField(primary_key=True)
+    username= models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    date_of_membership = models.DateField(auto_add_now=True)
+    active_status = models.BooleanField(default=True)
+    check_out_date = models.DateTimeField(default=True)
+    return_date = models.DateTimeField(null=True, blank=True)
