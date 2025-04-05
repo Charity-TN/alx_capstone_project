ad4ba0f49f174c0f992156da02da519e1a10485c
Charity Thuo <charitynthuo@gmail.com>
2025-04-05 10:41:02 +0300

    Removed the max_length in the Book class under te isbn attribute.

--- library_management_api/api/models.py
+++ library_management_api/api/models.py
@@ -7,7 +7,7 @@ class Book(models. Model):
     book_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=200)
-    isbn = models.IntegerField(max_length=13,unique=True)
+    isbn = models.IntegerField(unique=True)
     published_date = models.DateField()
     copies_available = models.PositiveIntegerField(default=1)
 
